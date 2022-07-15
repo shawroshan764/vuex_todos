90998d40b33f84492a96706540fc384408634642
Roshan Shaw <shawroshan764@gmail.com>
2022-07-15 12:27:44 -0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo/>
       <Todos/>
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    Todos
+    Todos,
+    AddTodo
   }
-}
+};
 </script>
 
 <style>
