3df000f83cf117bf3d0e634cbf8aa3803aa0e08f
Roshan Shaw <shawroshan764@gmail.com>
2022-07-15 13:15:02 -0700

    commit

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <h2>Todos</h2>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      <div
+        @dblclick="onDblClick(todo)"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        v-bind:class="{'is-complete':todo.completed}"
+      >
         {{ todo.title }}
-        <i @click ="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -12,15 +27,25 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+
 export default {
-  name: "TodosComponent",
+  name: "TodoComponent",
   methods: {
-    ...mapActions(["fetchTodos","deleteTodo"]),
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+
+      this.updateTodo(updTodo);
+    }
   },
   computed: mapGetters(["allTodos"]),
   created() {
     this.fetchTodos();
-  },
+  }
 };
 </script>
 
@@ -30,6 +55,7 @@ export default {
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 1rem;
 }
+
 .todo {
   border: 1px solid #ccc;
   background: #41b883;
@@ -39,11 +65,43 @@ export default {
   position: relative;
   cursor: pointer;
 }
-i{
+
+i {
   position: absolute;
   bottom: 10px;
-  right:10px;
-  color:#fff;
+  right: 10px;
+  color: #fff;
   cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
